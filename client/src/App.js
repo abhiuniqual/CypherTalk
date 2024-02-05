ed0eb72b0a96f7349a8a9a5df7bd0eb55112657a
abhi.uniqual <abhi.uniqual@gmail.com>
2024-02-05 16:14:51 +0530

    feat: Adding read receipts and online status

--- client/src/App.js
+++ client/src/App.js
@@ -8,14 +8,24 @@ import {
   Avatar,
   Container,
 } from "@mui/material";
+import { IoCheckmark, IoCheckmarkDone, IoSend } from "react-icons/io5";
 
 const socket = io.connect("http://localhost:3001");
 
 function App() {
+  const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
-  const [username, setUsername] = useState("");
+  const [onlineUsers, setOnlineUsers] = useState({});
+  const [readReceipts, setReadReceipts] = useState({});
+  const [messagesReadByRecipient, setMessagesReadByRecipient] = useState([]);
+
+  const markMessageAsRead = (index) => {
+    const updatedReadReceipts = { ...readReceipts };
+    updatedReadReceipts[index] = true;
+    setReadReceipts(updatedReadReceipts);
+  };
 
   const joinRoom = () => {
     if (room !== "" && username !== "") {
@@ -24,12 +34,54 @@ function App() {
   };
 
   const sendMessage = () => {
-    socket.emit("send_message", { message, room, username });
+    const newMessageIndex = messages.length;
+    socket.emit("send_message", {
+      message,
+      room,
+      username,
+      index: newMessageIndex,
+    });
   };
+
+  useEffect(() => {
+    socket.on("user_online", (data) => {
+      setOnlineUsers((prevUsers) => ({
+        ...prevUsers,
+        [data.username]: { status: "online", color: "#00FF00" },
+      }));
+    });
+
+    socket.on("user_offline", (data) => {
+      setOnlineUsers((prevUsers) => ({
+        ...prevUsers,
+        [data.username]: { status: "offline", color: "#FF0000" },
+      }));
+    });
+
+    socket.on("reconnect", (attemptNumber) => {
+      console.log(`Reconnected after ${attemptNumber} attempts`);
+      socket.emit("rejoin_room", { room, username });
+    });
+
+    socket.on("message_read", (data) => {
+      markMessageAsRead(data.index);
+      setMessagesReadByRecipient((prevMessages) => [
+        ...prevMessages,
+        data.index,
+      ]);
+    });
+
+    return () => {
+      socket.off("user_online");
+      socket.off("user_offline");
+      socket.off("message_read");
+    };
+  }, [socket, onlineUsers, readReceipts]);
 
   useEffect(() => {
     const handleReceiveMessage = (data) => {
       setMessages((prevMessages) => [...prevMessages, data]);
+      socket.emit("message_received", { index: messages.length, room });
     };
 
     socket.on("receive_message", handleReceiveMessage);
@@ -37,70 +89,103 @@ function App() {
     return () => {
       socket.off("receive_message", handleReceiveMessage);
     };
-  }, [socket]);
-
-  // useEffect(() => {
-  //   socket.on("receive_message", (data) => {
-  //     setMessages((prevMessages) => [...prevMessages, data]);
-  //   });
-  // }, [socket]);
+  }, [socket, messages, room]);
 
   return (
     <Container
-      maxWidth="xl"
-      sx={{ paddingLeft: "0 !important", paddingRight: "0 !important" }}
+      maxWidth="md"
+      sx={{ my: 2, paddingLeft: "0 !important", paddingRight: "0 !important" }}
     >
-      <Box display="flex" flexDirection="column" alignItems="center">
-        <Box width="400px">
-          <TextField
-            fullWidth
-            type="text"
-            placeholder="Your Name..."
-            onChange={(event) => {
-              setUsername(event.target.value);
-            }}
-          />
-        </Box>
-        <Box width="400px" display="flex" gap={2} mt={2}>
-          <TextField
-            placeholder="Room Number..."
-            onChange={(event) => {
-              setRoom(event.target.value);
-            }}
-          />
-          <Button onClick={joinRoom} variant="contained" color="warning">
-            Join Room
-          </Button>
-        </Box>
-        <Box width="400px" display="flex" gap={2} mt={2}>
-          <TextField
-            type="text"
-            placeholder="Message..."
-            onKeyPress={(event) => {
-              if (event.key === "Enter") {
-                sendMessage();
-              }
-            }}
-            onChange={(event) => {
-              setMessage(event.target.value);
-            }}
-          />
-          <Button onClick={sendMessage} variant="contained" color="secondary">
-            Send Message
-          </Button>
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        mt={4}
+        p={3}
+        bgcolor="#f5f5f5"
+        borderRadius={2}
+      >
+        <Typography variant="h5" mb={2} color="black">
+          Chat WebApp
+        </Typography>
+        <TextField
+          fullWidth
+          type="text"
+          variant="outlined"
+          placeholder="Your Name..."
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            gap: 1,
+            width: "100%",
+            flexDirection: {
+              md: "row",
+              xs: "column",
+            },
+          }}
+        >
+          <Box
+            display="flex"
+            flexDirection="column"
+            gap={2}
+            mt={2}
+            sx={{ width: { md: "50%", xs: "100%" } }}
+          >
+            <TextField
+              variant="outlined"
+              placeholder="Room Number..."
+              onChange={(event) => {
+                setRoom(event.target.value);
+              }}
+            />
+            <Button onClick={joinRoom} variant="contained" color="primary">
+              Join Room
+            </Button>
+          </Box>
+          <Box
+            display="flex"
+            flexDirection="column"
+            gap={2}
+            mt={2}
+            sx={{ width: { md: "50%", xs: "100%" } }}
+          >
+            <TextField
+              type="text"
+              variant="outlined"
+              placeholder="Message..."
+              onKeyPress={(event) => {
+                if (event.key === "Enter") {
+                  sendMessage();
+                }
+              }}
+              onChange={(event) => {
+                setMessage(event.target.value);
+              }}
+            />
+            <Button onClick={sendMessage} variant="contained" color="secondary">
+              Send Message <IoSend style={{ marginLeft: 6 }} fontSize={18} />
+            </Button>
+          </Box>
         </Box>
       </Box>
 
       <Container
         maxWidth="sm"
         sx={{
+          mt: 4,
           paddingLeft: "0 !important",
           paddingRight: "0 !important",
-          marginTop: 2,
         }}
       >
-        <Typography variant="h5">Messages:</Typography>
-        <Box sx={{ mx: 4, mt: 2 }}>
+        <Typography variant="h5" textAlign="center">
+          Messages:
+        </Typography>
+        <Box mt={2}>
           {messages.map((msg, index) => (
             <Box
               key={index}
@@ -109,37 +194,77 @@ function App() {
                 alignItems: "center",
                 justifyContent:
                   msg.username === username ? "flex-end" : "flex-start",
-                marginTop: "5px",
+                marginTop: "10px",
               }}
             >
               {msg.username !== username && (
+                <Box sx={{ mr: 2, position: "relative" }}>
+                  <Avatar
+                    sx={{
+                      backgroundColor:
+                        msg.username === username ? "#f50057" : "#3f51b5",
+                    }}
+                  >
+                    {msg.username ? msg.username[0] : ""}
+                  </Avatar>
+                  {onlineUsers[msg.username] && (
+                    <span
+                      style={{
+                        backgroundColor: onlineUsers[msg.username].color,
+                        width: "10px",
+                        height: "10px",
+                        borderRadius: "50%",
+                        position: "absolute",
+                        bottom: "0",
+                        right: "0",
+                      }}
+                    ></span>
+                  )}
+                </Box>
+              )}
+
+              <Box
+                sx={{
+                  borderRadius: 2,
+                  p: "6px 12px",
+                  mr: 1,
+                  backgroundColor:
+                    msg.username === username ? "#e3f2fd" : "#fce4ec",
+                }}
+              >
+                <Typography variant="body1">{`${
+                  msg.message || ""
+                }`}</Typography>
+              </Box>
+
+              {msg.username === username && (
                 <Avatar
                   sx={{
                     backgroundColor:
-                      msg.username === username ? "#3f51b5" : "#f50057",
+                      msg.username === username ? "#f50057" : "#3f51b5",
                   }}
                 >
                   {msg.username ? msg.username[0] : ""}
                 </Avatar>
               )}
 
-              <Typography
-                variant="body1"
-                sx={{
-                  ml: msg.username === username ? 0 : 2,
-                  mr: msg.username === username ? 2 : 0,
-                }}
-              >{`${msg.message || ""}`}</Typography>
-
               {msg.username === username && (
-                <Avatar
+                <Box
                   sx={{
-                    backgroundColor:
-                      msg.username === username ? "#3f51b5" : "#f50057",
+                    ml: 1,
+                    display: "flex",
+                    alignItems: "end",
+                    color: messagesReadByRecipient.includes(index)
+                      ? "#00FF00"
+                      : "#808080",
                   }}
                 >
-                  {msg.username ? msg.username[0] : ""}
-                </Avatar>
+                  {messagesReadByRecipient.includes(index) ? (
+                    <IoCheckmarkDone fontSize={16} />
+                  ) : (
+                    <IoCheckmark fontSize={16} />
+                  )}
+                </Box>
               )}
             </Box>
           ))}
--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ const io = new Server(server, {
 });
 
 const joinedUsers = [];
+const onlineUsers = {};
 
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
@@ -26,10 +27,45 @@ io.on("connection", (socket) => {
       io.to(data.room).emit("user_joined", { username: data.username });
       joinedUsers.push(socket.id);
     }
+
+    onlineUsers[socket.id] = data.username;
+    io.emit("user_online", { username: data.username });
   });
 
   socket.on("send_message", (data) => {
     io.to(data.room).emit("receive_message", data);
+
+    io.emit("user_online", { username: onlineUsers[socket.id] });
+  });
+
+  socket.on("message_received", (data) => {
+    socket.to(data.room).emit("message_read", {
+      index: data.index,
+      recipient: socket.id,
+    });
+  });
+
+  socket.on("rejoin_room", (data) => {
+    const { room, username } = data;
+    socket.join(room);
+
+    if (!joinedUsers.includes(socket.id)) {
+      io.to(room).emit("user_joined", { username });
+      joinedUsers.push(socket.id);
+    }
+
+    onlineUsers[socket.id] = username;
+    io.emit("user_online", { username });
+
+    io.to(room).emit("user_online", { username });
+  });
+
+  socket.on("disconnect", () => {
+    const username = onlineUsers[socket.id];
+    if (username) {
+      delete onlineUsers[socket.id];
+      io.emit("user_offline", { username });
+    }
   });
 });
 
